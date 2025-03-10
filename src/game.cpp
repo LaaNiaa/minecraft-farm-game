59be397d0bcc0c47b11a03a7b627007a41d347b8
LaaNiaa <laaniaa262626@gmail.com>
2025-03-10 22:30:38 +0100

    display one grass block on the screen

--- src/game.cpp
+++ src/game.cpp
@@ -1,20 +1,43 @@
 #include "../include/game.hpp"
 
+#include <iostream>
+
 Game::Game()
-    : window(sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Minecraft Farm Game")) {
+    : window(sf::RenderWindow(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Minecraft Farm Game")) {
     window.setFramerateLimit(60);
 
-    while (window.isOpen())
-    {
-        while (const std::optional<sf::Event> event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
+    loadTextures();
+}
 
-        window.clear();
-        window.display();
+void Game::loadTextures() {
+    if (!textures.Grass_Block.loadFromFile("../../textures/Grass_Block.png")) {
+        std::cerr << "Failed to load Grass_Block texture" << std::endl;
     }
+}
+
+void Game::run() {
+    while (window.isOpen()) {
+        processEvents();
+        render();
+
+        sf::sleep(sf::seconds(10));
+    }
+}
+
+void Game::processEvents() {
+    std::optional<sf::Event> event;
+    while (window.pollEvent()) {
+        if (event->is<sf::Event::Closed>()) {
+            window.close();
+        }
+    }
+}
+
+void Game::render() {
+    window.clear();
+
+    sf::Sprite sprite(textures.Grass_Block);
+    window.draw(sprite);
+
+    window.display();
 }
