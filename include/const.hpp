94369c362c0166d26c0c5c7f79913e17321b7b02
LaaNiaa <laaniaa262626@gmail.com>
2025-04-30 13:10:25 +0200

    set tile size depending on initial window size

--- include/const.hpp
+++ include/const.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-const int TILE_SIZE = 150;
 const int GRID_WIDTH = 10;
 const int GRID_HEIGHT = 10;
 const int WINDOW_WIDTH = 1920;
 const int WINDOW_HEIGHT = 1080;
+const int TILE_SIZE = WINDOW_WIDTH / 19.2;
 
 enum class BlockType {
     NONE,
