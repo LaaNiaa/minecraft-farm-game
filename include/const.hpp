c73eee40a88252f0dd79cd7c68834ce4e080058c
LaaNiaa <laaniaa262626@gmail.com>
2025-05-04 18:54:07 +0200

    change tile size

--- include/const.hpp
+++ include/const.hpp
@@ -4,7 +4,7 @@ const int GRID_WIDTH = 10;
 const int GRID_HEIGHT = 10;
 const int WINDOW_WIDTH = 1920;
 const int WINDOW_HEIGHT = 1080;
-const int TILE_SIZE = WINDOW_WIDTH / 19.2;
+const int TILE_SIZE = WINDOW_WIDTH / 12.8;
 
 enum class BlockType {
     NONE,
